54909f1bb9a2183993f7cf32b5752f4c6842ca74
Meldgaard <cm@geekmedia.dk>
2019-04-04 10:37:00 +0200

    Added required fields to construct

--- src/Form/RecaptchaForm.php
+++ src/Form/RecaptchaForm.php
@@ -2,11 +2,14 @@
 
 namespace Lundco\SilverStripe\ReCaptcha\Form;
 
+use SilverStripe\Control\RequestHandler;
+use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\Form;
+use SilverStripe\Forms\Validator;
 
 class RecaptchaForm extends Form
 {
-    public function __construct($controller,$name,$formFields,$formActions){
+    public function __construct(RequestHandler $controller,$name,FieldList $formFields,FieldList $formActions, Validator $validator = null){
 
         $formFields->push(RecaptchaField::create());
 
